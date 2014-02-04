updates to hmi data, explicit colormap, fix to rotation of map and new function to return unsigned magnetic flux

sunpy/cm/cm.py
@@ -58,6 +58,7 @@ cmlist = {
           'sdoaia1600': sdoaia1600,
           'sdoaia1700': sdoaia1700,
           'sdoaia4500': sdoaia4500,
+          'sdohmimag': cm.Greys,
           'sohoeit171': sohoeit171,
           'sohoeit195': sohoeit195,
           'sohoeit284': sohoeit284,
@@ -141,10 +142,11 @@ def show_colormaps():
     a = np.linspace(0, 1, 256).reshape(1, -1)  # pylint: disable=E1103
     a = np.vstack((a, a))
 
-    fig = plt.figure(figsize=(5, 10))
+    fig = plt.figure()
     fig.subplots_adjust(top=0.99, bottom=0.01, left=0.2, right=0.99)
+    fig.subplots_adjust(wspace=.5)
     for i, name in enumerate(maps):
-        ax = plt.subplot(nmaps, 1, i + 1)
+        ax = plt.subplot(nmaps, 2, i + 1)
         plt.axis("off")
         plt.imshow(a, aspect='auto', cmap=get_cmap(name), origin='lower')
         pos = list(ax.get_position().bounds)

sunpy/map/sources/sdo.py
@@ -6,6 +6,7 @@ __email__ = "keith.hughitt@nasa.gov"
 
 import numpy as np
 from matplotlib import colors
+from astropy.units import Quantity
 
 from sunpy.map import GenericMap
 from sunpy.cm import cm
@@ -68,13 +69,41 @@ class HMIMap(GenericMap):
         
         GenericMap.__init__(self, data, header, **kwargs)
         
+        # Mask the array of all nans
+        
         self.meta['detector'] = "HMI"
 #        self.meta['instrme'] = "HMI"
 #        self.meta['obsrvtry'] = "SDO"
 
         self._name = self.detector + " " + str(self.measurement)
         self._nickname = self.detector
-    
+        self.cmap = cm.get_cmap('sdohmimag')
+        #the following should really be happening in map base!
+        self.data = self.rotate(np.deg2rad(self.rotation_angle.get('y'))).data
+        self.data = np.ma.masked_array(self.data,np.isnan(self.data))
+
+        # next set crota2 to zero
+        
+    def unsigned_magnetic_flux(self):
+        """Return the unsigned magnetic flux for the map"""
+        # the following value should probably be available in map
+        arcsec_to_m = np.sin(np.deg2rad(1/(60.*60.))) * Quantity(self.dsun, 'm')
+        pixel_area = arcsec_to_m**2 * self.scale['x'] * self.scale['y']
+        xx, yy = self.pixel_to_data()
+        angles = np.sqrt(xx**2 + yy**2)/self.rsun_arcseconds * 90.0
+        angles = np.ma.masked_array(angles,np.isnan(angles))
+        pixel_angles = np.sqrt(xx**2 + yy**2)/self.rsun_arcseconds * 90.0
+        factor = 1/np.cos(np.deg2rad(angles))**2
+        bflux = pixel_area.to('cm**2') * Quantity(self.data,'gauss') * factor.transpose()
+        return np.sum(np.abs(bflux))
+
+    def _get_norm(self):
+        """Returns a Normalize object to be used with AIA data"""
+        # byte-scaled images have most likely already been scaled
+        if self.data.dtype == np.uint8:
+            return None
+        return colors.Normalize(-300, 300)
+
     @property
     def measurement(self):
         return self.meta['content'].split(" ")[0].lower()